8bbb00b3062201f67c1f5e223d8dc23e4528853e
Nainsee Chauhan <nainsee02@gmail.com>
2023-07-02 23:30:44 +0530

    first commit' git commit -m 'first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './App.css';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+
+function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode ('dark');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else{
+      setMode ('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+  return (
+    <>
+     <Navbar title = "TextUtils" about= "About" mode={mode} toggleMode={toggleMode}/>
+     <div className = "container my-3">
+     <TextForm heading = "Enter the text to analyse below" mode={mode}/>
+     </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/TextForm.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+    const handleLoClick = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    const handleClr = ()=>{
+        let newText = " ";
+        setText(newText);
+    }
+    const handleOnChange = (event)=>{
+       
+        setText(event.target.value)
+    }
+
+    const [text, setText] = useState('');
+
+    return (
+        <>
+        <div className = "container" style={{color : props.mode==='dark'?'white':'black'}} >
+            <h1>{props.heading}  </h1>
+            <div className="mb-3">
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor : props.mode==='dark'?'grey':'white',
+             color:props.mode==='dark'?'white':'#042743'}} id="myText" rows="8"></textarea>
+            </div>
+            <button className="btn btn-outline-primary mx-2 " onClick={handleUpClick}> Uppercase </button>
+            <button className="btn btn-outline-primary mx-1" onClick={handleLoClick}> Lowercase </button>
+            <button className="btn btn-outline-primary mx-1" onClick={handleClr}> Clear </button>
+          
+            
+        </div>
+        <div className = "container my-3" style={{color : props.mode==='dark'?'white':'black'}}>
+            <h3>Your text summary</h3>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <h4>Preview</h4>
+            <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+
+        </div>
+        </>
+    )
+}
